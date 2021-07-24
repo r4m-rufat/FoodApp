95eea8da4189f6c156d61b5d6e8c6b9ff9bfd0a0
Rufat Nasirov <rufet.nesirov2001@mail.ru>
2021-07-24 23:27:36 +0400

    image design

--- app/src/main/java/com/example/deliveryapp/HomeActivity.kt
+++ app/src/main/java/com/example/deliveryapp/HomeActivity.kt
@@ -3,13 +3,19 @@ package com.example.deliveryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
+import coil.compose.rememberImagePainter
 import com.example.deliveryapp.models.ResultsItem
 import com.example.deliveryapp.ui.theme.DeliveryAppTheme
 import com.example.deliveryapp.viewmodels.HomeActivityViewModel
@@ -23,22 +29,13 @@ class HomeActivity : ComponentActivity() {
 
         var viewModelProvider = ViewModelProvider(this).get(HomeActivityViewModel::class.java)
 
-//        viewModelProvider.getDeliveryList().observe(this, object : Observer<List<ResultsItem?>?>{
-//            override fun onChanged(t: List<ResultsItem?>?) {
-//                Log.d(TAG, "onChanged: Comes --> ${t?.get(0)?.title} ")
-//                deliveryList = t
-//
-//            }
-//
-//        })
-
         setContent {
             DeliveryAppTheme {
 
                 val deliveryList = viewModelProvider.deliveryList.value
 
                 deliveryList?.let {
-                    if (it != null){
+                    if (it != null) {
                         TextTest(recipes = it)
                     }
                 }
@@ -61,8 +58,17 @@ fun TextTest(
 
     Column(modifier = Modifier.fillMaxWidth()) {
 
-        for (recipe in recipes!!){
-            Text(text = recipe!!.title.toString())
+        for (recipe in recipes!!) {
+            Text(text = recipe!!.title.toString(), modifier = Modifier.padding(10.dp))
+            Image(
+                painter = rememberImagePainter(data = recipe!!.image.toString()),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop
+            )
         }
 
     }
