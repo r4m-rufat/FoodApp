998f80b89218a756a32c64b5408aae847aa0458d
Rufat Nasirov <rufet.nesirov2001@mail.ru>
2021-07-29 18:26:29 +0400

    html converter

--- app/src/main/java/com/example/deliveryapp/utils/convertors/HTMLConverter.kt
+++ app/src/main/java/com/example/deliveryapp/utils/convertors/HTMLConverter.kt
@@ -2,6 +2,9 @@ package com.example.deliveryapp.utils.convertors
 
 import org.jsoup.Jsoup
 
+/**
+ * changes html codes to string
+ */
 fun convertHTMLToString(html: String): String{
 
     return Jsoup.parse(html).text()
